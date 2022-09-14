647837f2ee9e1ad033266383e854fbbc8c6f4432
Roziikh <rozii@bk.ru>
2022-09-14 12:27:25 +0400

    adding

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import bodyParseer from 'body-parser'
+
+const app = express()
+const PORT = 5000
+
+app.use(bodyParseer.json());
+
+app.get('/', (req, res) => res.send ('Hello from Homepage.'));
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`))
